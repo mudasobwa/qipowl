0d85bcf616ca7930240fea4586ce46fc6a412770
Alexei Matyushkin <am@mudasobwa.ru>
2013-10-12 11:11:21 +0400

    Tests for Bowler.

---
+++ spec/hash_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Hash do
+  before(:each) do
+    @hash = {:'*'=>{'*'=>1, 'b'=>2},:w=>{:'*'=>:*, :b=>'b'}}
+    @res = {:ⓣ=>{'*'=>1, 'b'=>2},:w=>{:ⓣ=>:*, :b=>'b'}}
+  end
+
+  describe "#bowl" do
+    context 'when bowled' do
+      it 'replaces occurences recursively' do
+        expect(@hash.bowl).to eql @res
+      end
+    end
+    context 'when bowled and unbowled' do
+      it 'remains the same string' do
+        expect(@hash.bowl.unbowl).to eql @hash
+      end
+    end
+  end
+  describe "#bowl!" do
+    context 'when bowled inplace' do
+      it 'replaces occurences recursively within current instance' do
+        @hash.bowl!
+        expect(@hash).to eql @res
+      end
+    end
+  end
+end
+
