6b62c9024827dc7b8bbda6f40c1d144843d5cb5f
Alexei Matyushkin <am@mudasobwa.ru>
2014-01-19 19:58:52 +0400

    Looks like up and running (but js.)

--- extras/demo/main.rb
+++ extras/demo/main.rb
@@ -4,31 +4,34 @@ require 'cgi'
 require 'json'
 require 'sinatra'
 
-require_relative '../qipowl'
+require_relative '../../lib/qipowl'
 
 use Rack::Session::Pool, :expire_after => 2592000
 
 before do
-  session[:typo] ||= Qipowl.tg_md__html
+  session[:typo] ||= Qipowl::Ruler.new_bowler "html"
+  puts '-'*60
+  puts session[:typo].class::ENTITIES
+  puts '-'*60
 end
 
 get '/html/mapping' do
   content_type :json
-  session[:typo].mapping.hash.to_json
+  session[:typo].class::ENTITIES.to_json
 end
 
 delete '/html/mapping/:key' do |key|
   content_type :json
-  session[:typo].mapping.remove_spice(key.to_sym).to_json
+  session[:typo].remove_entity(key.to_sym).to_json
 end
 
 put '/html/mapping/:section/:key/:value/?:enclosure?' do |section, key, value, enclosure|
   content_type :json
-  session[:typo].mapping.add_spice(section.to_sym, key.to_sym, value.to_sym, enclosure ? enclosure.to_sym : nil).to_json
+  session[:typo].add_entity(section.to_sym, key.to_sym, value.to_sym, enclosure ? enclosure.to_sym : nil).to_json
 end
 
 get '/html/parse' do
   str = CGI::parse(request.query_string)['text'].first
   content_type :html
-  session[:typo].parse_and_roll str
+  session[:typo].execute str
 end
